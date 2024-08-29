11f698de18fda37a97d48960573a920e71ee7f8e
Vijay Nagane <128283255+VPrabhakarN@users.noreply.github.com>
2024-08-29 08:45:42 +0530

    Add files via upload

---
+++ Connection_to_db_3.java
@@ -0,0 +1,41 @@
+package connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class Practice3 {
+
+	public static void main(String[] args) {
+		
+		// URL, USER, PASS
+		String url = "jdbc:mysql://localhost:3306/employee";
+		String user = "root";
+		String pass = "VinnyyyMinnyyy@1923";
+		
+		// using try block
+		try {
+			
+			// establishing connection
+			Connection conn = DriverManager.getConnection(url, user, pass);
+			
+			// statement 
+			Statement st = conn.createStatement();
+			
+			// create query to execute
+			ResultSet rs = st.executeQuery("select * from employee1");
+			
+			// using while loop 
+			while(rs.next()) {
+				System.out.println(rs.getString("E_Name") + " " + rs.getInt("age"));
+			}
+			
+		} catch(SQLException e) { // catch block
+			System.out.println("Error : " + e);
+		}
+
+	}
+
+}
